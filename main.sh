802ed3a51282392318de8256bc93783e6ff98dcf
jbatool <112132821+javaria712@users.noreply.github.com>
2022-09-02 00:03:56 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,25 @@
+#Basic Git workflow
+#working directory,staging area, repository.
+#git init - git add-git-commit
+#step-1: To initialize Git repository
+# $git init
+#$git status
+#Check the status of file tracked file.checked the contents of the working directory and staging area.
+#$git add
+#In order to Git start tracking txt file the file needs to be added on staging area. $git add scene-1.txt
+#$git diff
+#difference between the working directory and the staging area. git diff filename.
+#$git commit
+#this will permenently stores changes from the staging area inside the repositry.
+#$git log
+#you can see commit with git log. 
+#git clone
+#give the name to repositry. it will change the name the name we provide.
+#git remot -v
+#if we want to see is source or orgin,location of the repositry we use.
+#$git fetch
+#if change in remote repository fetch will bring the changes.
+#$git merge
+#we merge the change with master branch. git merge orgin/master.
+#$git push
+#share our work. we push branch up to remote.
